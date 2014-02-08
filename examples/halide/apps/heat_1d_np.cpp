f37cf77d33d0483ad40b7e97e112cd677f2d5622
Charith Paranaliyanage <paranaliyanage@gmail.com>
2014-02-08 16:28:23 +0530

    heat 1d np program to generate halide schedules

--- examples/halide/apps/heat_1d_np.cpp
+++ examples/halide/apps/heat_1d_np.cpp
@@ -9,19 +9,19 @@ int main(int argc, char **argv) {
 	ImageParam input(Float(32), 2);
 	Var x("x"), y("y");
 
-	Func heat("heat");
+	Func heat_1d_np("heat");
 	RDom r(1, input.width() - 2, 1, input.height() - 1);
 
-	heat(x, y) = input(x, y);
-	heat(r.x, r.y) = 0.125f
-			* (heat(r.x - 1, r.y - 1) - 2.0f * heat(r.x, r.y - 1)
-					+ heat(r.x + 1, r.y - 1));
+	heat_1d_np(x, y) = input(x, y);
+	heat_1d_np(r.x, r.y) = 0.125f
+			* (heat_1d_np(r.x - 1, r.y - 1) - 2.0f * heat_1d_np(r.x, r.y - 1)
+					+ heat_1d_np(r.x + 1, r.y - 1));
 
 	AUTOTUNE_HOOK(heat);
-	heat.compute_root();
+	heat_1d_np.compute_root();
 	BASELINE_HOOK(heat);
 
-	heat.compile_to_file("heat_1d", input);
+	heat_1d_np.compile_to_file("heat_1d", input);
 
 	return 0;
 }
