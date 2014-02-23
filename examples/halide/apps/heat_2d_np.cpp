b7cc8b4f213c9731ddc9a301a5cbfd911d59360c
Charith Paranaliyanage <paranaliyanage@gmail.com>
2014-02-23 09:48:52 +0530

    modified halide programs to use a C loop which is calling a fixed-length Halide pipeline

--- examples/halide/apps/heat_2d_np.cpp
+++ examples/halide/apps/heat_2d_np.cpp
@@ -6,29 +6,38 @@ using namespace Halide;
 
 int main(int argc, char **argv) {
 
-	ImageParam input(Float(32), 3);
-	Var x("x"), y("y"), z("z");
+	ImageParam input(Float(32), 2);
 
-	Func heat("heat");
-	RDom r(1, input.width() - 2, 1, input.height() - 2, 1,
-			input.channels() - 1);
-
-	heat(x, y, z) = input(x, y, z);
-
-	heat(r.x, r.y, r.z) = 0.125f
-			* (heat(r.x + 1, r.y, r.z - 1) - 2.0f * heat(r.x, r.y, r.z - 1)
-					+ heat(r.x - 1, r.y, r.z - 1))
+	Var x("x"), y("y");
+	Func heat_0("heat_0"), clamped_0("clamped_0"), heat_1("heat_1"), clamped_1(
+			"clamped_1"), heat_2("heat_0"), clamped_2("clamped_0");
+	clamped_0(x, y) = input(clamp(x, 1, input.width() -2), clamp(y, 1, input.height() -2));
+	heat_0(x, y) = 0.125f
+			* (clamped_0(x + 1, y) - 2.0f * clamped_0(x, y)
+					+ clamped_0(x - 1, y))
 			+ 0.125f
-					* (heat(r.x, r.y + 1, r.z - 1)
-							- 2.0f * heat(r.x, r.y, r.z - 1)
-							+ heat(r.x, r.y - 1, r.z - 1))
-			+ heat(r.x, r.y, r.z - 1);
+					* (clamped_0(x, y + 1) - 2.0f * clamped_0(x, y)
+							+ clamped_0(x, y - 1)) + clamped_0(x, y);
 
-	AUTOTUNE_HOOK(heat);
-	heat.compute_root();
-	BASELINE_HOOK(heat);
+	clamped_1(x, y) = heat_0(clamp(x, 1, input.width() -2), clamp(y, 1, input.height() -2));
+	heat_1(x, y) = 0.125f
+			* (clamped_1(x + 1, y) - 2.0f * clamped_1(x, y)
+					+ clamped_1(x - 1, y))
+			+ 0.125f
+					* (clamped_1(x, y + 1) - 2.0f * clamped_1(x, y)
+							+ clamped_1(x, y - 1)) + clamped_1(x, y);
+	clamped_2(x, y) = heat_1(clamp(x, 1, input.width() -2), clamp(y, 1, input.height() -2));
+	heat_2(x, y) = 0.125f
+			* (clamped_2(x + 1, y) - 2.0f * clamped_2(x, y)
+					+ clamped_2(x - 1, y))
+			+ 0.125f
+					* (clamped_2(x, y + 1) - 2.0f * clamped_2(x, y)
+							+ clamped_2(x, y - 1)) + clamped_2(x, y);
 
-	heat.compile_to_file("heat_2d_np", input);
+	AUTOTUNE_HOOK(heat_2);
+	heat_2.compute_root();
+	BASELINE_HOOK(heat_2);
 
+	heat_2.compile_to_file("heat_2d_np", input);
 	return 0;
 }
--- examples/halide/run.sh
+++ examples/halide/run.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-for input1 in 32 64 128 256 512 1024 2048 4096 8192 16384 32768 65536
-#for input1 in 131072 262144 524288 1048576
+SOURCE_FILE=apps/heat_2d_np.cpp
+echo "Source is : $SOURCE_FILE"
+for input in 32 64 128 #256 512 1024 2048 4096 8192 16384 32768 65536 131072 262144 524288 1048576
     do
-        for a in `seq 2`
-            do
-				sleep 5
-                input2=`expr $input1 \\* $a`;
-                echo "Input: \"$input1 ,$input2\"";
-#				python halidetuner.py --stop-after=5 apps/heat_1d_np.cpp --halide-dir=../../../halide --label=heat --input-size=128,128 --limit=100
-                python halidetuner.py --stop-after=10000 apps/heat_1d_np.cpp --halide-dir=../../../halide --label=heat --input-size="$input1 ,$input2" --limit=100
-#               python halidetuner.py --stop-after=10000 apps/halide_heat2d.cpp --halide-dir=../../../halide --label=heat --input-size="$input1 ,$input2 ,$input2" --memory-limit=105601155200 --limit=100
-            done;
+		sleep 5
+        echo "Input: $input";
+        python halidetuner.py --stop-after=10000 $SOURCE_FILE --halide-dir=../../../halide --label=heat --input-size="$input,$input" --limit=100
+#       python halidetuner.py --stop-after=10000 apps/halide_heat2d.cpp --halide-dir=../../../halide --label=heat --input-size="$input1 ,$input2 ,$input2" --memory-limit=105601155200 --limit=100
     done
-
