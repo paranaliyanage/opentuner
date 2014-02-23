3f5428fdc9133677a7c1c82b5e71ad49d07a82b0
Charith Paranaliyanage <paranaliyanage@gmail.com>
2014-02-23 09:48:52 +0530

    modified halide programs to use a C loop which is calling a fixed-length Halide pipeline

--- examples/halide/apps/heat_1d_np.cpp
+++ examples/halide/apps/heat_1d_np.cpp
@@ -6,22 +6,25 @@ using namespace Halide;
 
 int main(int argc, char **argv) {
 
-	ImageParam input(Float(32), 2);
-	Var x("x"), y("y");
+	ImageParam input(Float(32), 1);
 
-	Func heat("heat");
-	RDom r(1, input.width() - 2, 1, input.height() - 1);
+	Var x("x");
+	Func heat_0("heat_0"), clamped_0("clamped_0"), heat_1("heat_1"), clamped_1(
+			"clamped_1"), heat_2("heat_0"), clamped_2("clamped_0");
+	clamped_0(x) = input(clamp(x, 1, input.width() -2));
+	heat_0(x) = 0.125f
+			* (clamped_0(x - 1) - 2.0f * clamped_0(x) + clamped_0(x + 1));
+	clamped_1(x) = heat_0(clamp(x, 1, input.width() -2));
+	heat_1(x) = 0.125f
+			* (clamped_1(x - 1) - 2.0f * clamped_1(x) + clamped_1(x + 1));
+	clamped_2(x) = heat_1(clamp(x, 1, input.width() -2));
+	heat_2(x) = 0.125f
+			* (clamped_2(x - 1) - 2.0f * clamped_2(x) + clamped_2(x + 1));
 
-	heat(x, y) = input(x, y);
-	heat(r.x, r.y) = 0.125f
-			* (heat(r.x - 1, r.y - 1) - 2.0f * heat(r.x, r.y - 1)
-					+ heat(r.x + 1, r.y - 1));
+	AUTOTUNE_HOOK(heat_2);
+	heat_2.compute_root();
+	BASELINE_HOOK(heat_2);
 
-	AUTOTUNE_HOOK(heat);
-	heat.compute_root();
-	BASELINE_HOOK(heat);
-
-	heat.compile_to_file("heat_1d", input);
-
+	heat_2.compile_to_file("heat_1d_np", input);
 	return 0;
 }
